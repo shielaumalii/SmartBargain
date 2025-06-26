972697009f2e357c6115fe82c9229d29745104fb
Shiela Umali <shielamarie.lleva.umali@gmail.com>
2025-06-26 19:32:23 +1200

    Added frontend elements to homepage

--- check_columns.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('core/db/smartbargain.db')
-cursor = conn.cursor()
-
-print("Columns in negotiation_dashboard:")
-cursor.execute("PRAGMA table_info(negotiation_dashboard);")
-for column in cursor.fetchall():
-    print(column)
